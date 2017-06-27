0eff85e7df9caabb87d8202e619b4e2c7ca38e43
yxie <yxie@PATCAV195>
2017-06-27 16:25:04 -0400

    PIE p23

---
+++ CPPAcc/Source_PIE.cpp
@@ -0,0 +1,8 @@
+/*
+	The Basic Steps
+		1. Make sure you understand the problem
+		2. When you understand the question, try a simple example
+		3. Focus on the algorithm and data structures you will use to solve the problem
+			“I’m wondering whether I can store the values in an array and then sort them, but I don’t think that will work because I can’t quickly look up elements in an array by value.”
+		4. After you figure out your algorithm and how you can implement it, explain your solution to the interviewer
+*/
